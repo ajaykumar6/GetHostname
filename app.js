1eaad96401e5a4bca609315aca2044c5c79038ea
Ajay kumar <ajaykumarmanu221@gmail.com>
2021-02-12 12:18:41 +0530

    first commit

--- app.js
+++ app.js
@@ -6,5 +6,6 @@ var handler = function(request, response) {
  response.writeHead(200);
  response.end("You've hit " + os.hostname() + "\n");
 };
+
 var www = http.createServer(handler);
 www.listen(8080);
